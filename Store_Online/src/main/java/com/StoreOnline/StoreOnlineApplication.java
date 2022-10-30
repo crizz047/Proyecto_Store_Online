494262138fc813dc16badec902db0ce921b454cd
Cristian Zanabria Alvarez <zanabriaalvarezcristianmiguel@gmail.com>
2022-10-30 00:59:47 -0500

    Este es el primer commit del Proyecto Store Online

---
+++ Store_Online/src/main/java/com/StoreOnline/StoreOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.StoreOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreOnlineApplication.class, args);
+	}
+
+}
